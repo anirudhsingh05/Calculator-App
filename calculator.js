c6161ad90326fe87f5e1a5fc97dad5f39c796dd1
anirudhsingh05 <anirudhsingh05032001@gmail.com>
2023-11-20 18:18:03 +0530

    Fixed some bugs

--- calculator.js
+++ calculator.js
@@ -1,11 +1,12 @@
-
-let buffer = "0";
+let buffer = '0';
 let runningTotal = 0;
 let previousOp = null;
-const screen = document.querySelector('.answer-screen');
+
+let screen = document.querySelector('.answer-screen');
+
 
 function buttonClick(value) {
-    if(isNaN(parseInt(value))){
+    if(isNaN(parseInt(value)) ) {
         handleSymbol(value);
     } else {
         handleNumber(value);
@@ -13,15 +14,31 @@ function buttonClick(value) {
     rerender();
 }
 
-function handleNumber(number){
-    if(buffer === "0"){
+function handleNumber(number) {
+    if(buffer === '0'){
         buffer = number;
     } else {
         buffer += number;
     }
 }
 
-function flushOperator(intBuffer) {
+function handleMath(symbol) {
+    if(symbol === '0'){
+        return;
+    } 
+
+    const intBuffer = parseInt(buffer);
+
+    if(runningTotal === 0){
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+    previousOp = symbol;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer) {
     if(previousOp === '+'){
         runningTotal += intBuffer;
     } else if(previousOp === '-'){
@@ -33,63 +50,48 @@ function flushOperator(intBuffer) {
     }
 }
 
-function handleMath(value) {
-    if(buffer === 0){
-        return;
-    }
-
-    const intBuffer = parseFloat(buffer);
-    if(runningTotal === 0){
-        runningTotal = intBuffer;
-    } else {
-        flushOperator(intBuffer);
-    }
-    previousOp = value;
-    buffer = '0';
-}
-
-function handleSymbol(symbol){
+function handleSymbol(symbol) {
     switch(symbol) {
         case 'C':
-            buffer = "0";
-            break;
-        case '=':
-            if(previousOp === null){
-                return;
-            }
-            flushOperator(parseInt(buffer));
-            previousOp = null;
-            buffer = ""+runningTotal;
-            runningTotal = 0;
+            buffer = '0';
             break;
         case '←':
             if(buffer.length === 1){
                 buffer = '0';
             } else {
-                buffer = buffer.substring(0, buffer.length - 1);
+                buffer = buffer.substring(buffer.length-1);
             }
             break;
-        case '÷':
-        case '+':
-        case 'x':
-        case '-':
-            handleMath(symbol);
+        case '=':
+            if(previousOp === null) {
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            // when = is clicked previous operator will be null
+            previousOp = null;
+            // buffer will be string and assigned buffer what was the total up till now
+            buffer = "" + runningTotal;
+            // after = is clicked answer is displayed and nothing is running
+            runningTotal = 0;
             break;
-        case '.':
-            buffer += '.';
+        case '+':
+        case '-':
+        case 'x':
+        case '÷':
+            handleMath(symbol);
             break;
     }
 }
 
-function init() {
-    document.querySelector('.buttons')
-    .addEventListener("click", function(event) {
-        buttonClick(event.target.innerText);
-    });
+function rerender() {
+    screen.innerText = buffer;
 }
 
-function rerender(){
-    screen.innerText = buffer;
+function init() {
+    document.querySelector('.buttons')
+    .addEventListener('click', function(event) {
+        buttonClick(event.target.innerText);
+    })
 }
 
 init();
